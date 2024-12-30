5c0cdc059b35df35719796468bcfba9f21dfe7d8
surafeldev <surafel.nigusie.dev@gmail.com>
2024-12-30 16:56:45 +0300

    add new config to enable or disable automatic transfers

--- src/configuration/permit-generation-configuration.ts
+++ src/configuration/permit-generation-configuration.ts
@@ -1,5 +1,7 @@
 import { Static, Type } from "@sinclair/typebox";
 
-export const permitGenerationConfigurationType = Type.Object({});
+export const permitGenerationConfigurationType = Type.Object({
+    automaticTransfersMode: T.Boolean({ default: false, description: "Enable or disable automatic transfers mode" }),
+});
 
 export type PermitGenerationConfiguration = Static<typeof permitGenerationConfigurationType>;
